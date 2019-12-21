efcaf393be311acd2c94728aef20d2654bbac04b
Markus Kowalewski <markus.kowalewski@gmail.com>
2019-12-21 19:13:53 +0100

    dablin: 1.11.0 -> 1.12.0

--- dablin/default.nix
+++ dablin/default.nix
@@ -3,7 +3,7 @@
 } :
 
 let
-  version = "1.11.0";
+  version = "1.12.0";
 
 in stdenv.mkDerivation {
   name = "dablin-${version}";
@@ -12,7 +12,7 @@ in stdenv.mkDerivation {
     owner = "Opendigitalradio";
     repo = "dablin";
     rev = "${version}";
-    sha256 = "04ir7yg7psnnb48s1qfppvvx6lak4s8f6fqdg721y2kd9129jm82";
+    sha256 = "0d514ixz062xyyh4k3laxwhn3k3a1l4jq4w7rxf8x46d3743zrf7";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
