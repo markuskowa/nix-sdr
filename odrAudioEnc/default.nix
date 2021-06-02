b7473af433f4ce0a3dd13be82688651f4649d0ef
Markus Kowalewski <markus.kowalewski@gmail.com>
2021-06-02 23:44:44 +0200

    odrAudioEnc: 3.0.0 -> 3.1.0

--- odrAudioEnc/default.nix
+++ odrAudioEnc/default.nix
@@ -1,21 +1,22 @@
 { stdenv, fetchFromGitHub, autoconf, automake, libtool
-, pkgconfig, zeromq, alsaLib, vlc, curl
+, pkg-config, zeromq, alsaLib, vlc, curl
 } :
 
 let
-  version = "3.0.0";
+  version = "3.1.0";
 
 in stdenv.mkDerivation {
-  name = "odrAudioEnc-${version}";
+  pname = "odrAudioEnc";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "Opendigitalradio";
     repo = "ODR-AudioEnc";
     rev = "v${version}";
-    sha256 = "1v0ipw194alijaklfg7lq8hypiay7k09g1hqvp5g0w04g25ka4gn";
+    sha256 = "1jgagblhy3q384wz6r90wl1yfk3an7vsry8dwc0a0ld6d653hi9k";
   };
 
-  nativeBuildInputs = [ autoconf automake libtool pkgconfig ];
+  nativeBuildInputs = [ autoconf automake libtool pkg-config ];
   buildInputs = [ zeromq alsaLib vlc curl ];
 
   configureFlags = [
