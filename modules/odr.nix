9ac2da15c79781ccbf69aaeb63243cd10b5a1f5f
Markus Kowalewski <markus.kowalewski@gmail.com>
2021-06-08 23:47:51 +0200

    nixos/odr: remove global enable

--- modules/odr.nix
+++ modules/odr.nix
@@ -8,19 +8,5 @@ with lib;
     ./odr-dabmux.nix
     ./odr-dabmod.nix
   ];
-
-  options.services.odr.enable = mkEnableOption "Opendigital Radio components";
-
-  config = mkIf config.services.odr.enable {
-
-    # No need to run as root
-    users.users.odruser = {
-      description   = "ODR daemon user";
-      isSystemUser  = true;
-      group         = "odrgroup";
-    };
-
-    users.groups.odrgroup = {};
-  };
 }
 
