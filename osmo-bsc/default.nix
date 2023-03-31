36fd65a7e65e75ff212655551ad38da7a2fd956d
Markus Kowalewski <markus.kowalewski@gmail.com>
2023-04-01 01:34:53 +0200

    osmo-bsc: 1.9.0 -> 1.10.0

--- osmo-bsc/default.nix
+++ osmo-bsc/default.nix
@@ -6,12 +6,12 @@
 
 stdenv.mkDerivation rec {
   pname = "osmo-bsc";
-  version = "1.9.0";
+  version = "1.10.0";
 
   src = fetchgit {
     url = "https://gitea.osmocom.org/cellular-infrastructure/osmo-bsc";
     rev = version;
-    sha256 = "sha256-RxkyJ7XP16jIHdkTH0i8N6UREemJHx6fv6xX9sFyldg=";
+    sha256 = "sha256-OAYvelHaGzQxUgViqH4PW6SQfCQzPUqxVhr6qTq0y7M=";
   };
 
   nativeBuildInputs = [
