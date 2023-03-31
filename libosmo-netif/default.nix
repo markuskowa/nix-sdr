fbbdb0a86420f8e18ec564605117a7b8e0b05ff5
Markus Kowalewski <markus.kowalewski@gmail.com>
2023-04-01 01:15:29 +0200

    libosmo-netif: 1.2.0 -> 1.3.0

--- libosmo-netif/default.nix
+++ libosmo-netif/default.nix
@@ -5,12 +5,12 @@
 
 stdenv.mkDerivation rec {
   pname = "libosmo-netif";
-  version = "1.2.0";
+  version = "1.3.0";
 
   src = fetchgit {
     url = "https://gitea.osmocom.org/osmocom/libosmo-netif";
     rev = version;
-    sha256 = "sha256-Zy4wgEztY8fb9BzkUmMKwnFemgeae8MEp48OZhitS8I=";
+    sha256 = "sha256-PhGi/6JVO8tXxzfGwEKUB/GdrgCJkqROo26TPU+O9Sg=";
   };
 
   postPatch = ''
